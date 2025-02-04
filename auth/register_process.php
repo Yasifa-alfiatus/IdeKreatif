154d303ce28bd1ba69fea1cfec6d1855c1498083
Yasifa-alfiatus <yasifaalfiatus13@smk.belajar.id>
2025-02-04 12:13:42 +0700

    register proses

--- auth/register_process.php
+++ auth/register_process.php
@@ -6,7 +6,7 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $name = $_POST["name"];
-    $password = $_post["password"];
+    $password = $_POST["password"];
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
     $sql = " INSERT INTO users (username, name, password)
